9175352daecddf89b720ce392da5d18483f63199
AnushaKAntony <anushakantony@gmail.com>
2024-06-13 04:29:10 +0530

    project completed

--- src/components/SearchHosteller.jsx
+++ src/components/SearchHosteller.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useState } from 'react'
+import NavBar from './NavBar'
 
 const SearchHosteller = () => {
     const [data, setData] = useState(
@@ -46,7 +47,7 @@ const SearchHosteller = () => {
     }
     return (
         <div>
-            {/* <NavBar /> */}
+            <NavBar/> 
             <br></br><h3><u><center>Search Hostellers</center></u></h3><br></br>
             <div className="container">
                 <div className="row">
